231b12b037ac585b39df5ef5d1b4a970c3798c3b
alafia98 <alafiakhadija00@gmail.com>
2023-07-14 20:04:36 +0100

    finish

---
+++ client/src/components/Prices.js
@@ -0,0 +1,22 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "0 to 99 MAD",
+    array: [0, 99],
+  },
+  {
+    _id: 1,
+    name: "100 to 499 MAD",
+    array: [100, 499],
+  },
+  {
+    _id: 2,
+    name: "500 to 999 MAD",
+    array: [500, 999],
+  },
+  {
+    _id: 3,
+    name: "1000 MAD to more",
+    array: [1000, 9999],
+  }
+];
